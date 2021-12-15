707370822ab57e0aeed2cf55e8b8a12bc5af0b34
Cole Rau <colevrau@gmail.com>
2021-12-14 19:25:28 -0800

    trying to get Checkout API call working

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,5 +1,33 @@
+require 'square'
+require 'securerandom'
+
 class CheckoutController < ApplicationController
   def checkout
-    binding.pry
+    client = Square::Client.new(
+    access_token: ENV['API_ACCESS_TOKEN'],
+    environment: 'sandbox')
+
+    result = client.checkout.create_checkout(
+    location_id: ENV['LOCATION_ID'],
+    body: {
+      idempotency_key: SecureRandom.uuid,
+      order: {},
+      ask_for_shipping_address: true
+    })
+
+    if result.success?
+      puts result.data
+      binding.pry
+    elsif result.error?
+      warn result.errors
+      binding.pry
+    end
+  end
+
+
+  private
+
+  def checkout_params
+    params.require(:checkout).permit(:category, :type)
   end
 end
