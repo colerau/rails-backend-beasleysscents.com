1670e2bae12dbc4f4139206c1400428e1d6e5e40
Cole Rau <colevrau@gmail.com>
2021-12-16 13:27:40 -0800

    added Product model and Products controller, seeded database with bubble candles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Sweet Orange  (Tint of Orange Bubble Candle or Plain)
+# Peppermint & Lemon (Pastel Green Bubble Candle or plain)
+# Lavender & Lemon ( Pastel Purple Bubble Candle or Plain)
+# Lavender ( Pastel Purple Bubble Candle or Plain)
+# Coconut & Mango (Tint of Orange Bubble Candle or Plain)
+# Vanilla Rose  (Pastel Pink Bubble Candle)
+# Peppermint Vanilla (Pastel Green Bubble Candle or plain)
+# Warm Vanilla Kiss
+# Apple Cider
+# Apple Cinnamon spice (Pastel Tan Bubble Candle)
+# Vanilla Latte (Pastel Tan Bubble Candle or plain)
+# Brown Sugar Plum
+# Cinnamon Pumpkin Pie
+# Pumpkin & Vanilla
+# Gingerbread (Pastel Tan Bubble Candle or plain)
+# Pumpkin Spice Latte (Tint of Orange Bubble Candle or Plain)
+
+bubble_candle_1 = Product.create(category: "Bubble Candle", scent: "Sweet Orange", color: "Tint of Orange")
+bubble_candle_2 = Product.create(category: "Bubble Candle", scent: "Sweet Orange", color: "Plain")
+bubble_candle_3 = Product.create(category: "Bubble Candle", scent: "Peppermint & Lemon", color: "Pastel Green")
+bubble_candle_4 = Product.create(category: "Bubble Candle", scent: "Peppermint & Lemon", color: "Plain")
+bubble_candle_5 = Product.create(category: "Bubble Candle", scent: "Lavender & Lemon", color: "Pastel Purple")
+bubble_candle_6 = Product.create(category: "Bubble Candle", scent: "Lavender & Lemon", color: "Plain")
+bubble_candle_7 = Product.create(category: "Bubble Candle", scent: "Lavender", color: "Pastel Purple")
+bubble_candle_8 = Product.create(category: "Bubble Candle", scent: "Lavender", color: "Plain")
+bubble_candle_9 = Product.create(category: "Bubble Candle", scent: "Coconut & Mango", color: "Tint of Orange")
+bubble_candle_10 = Product.create(category: "Bubble Candle", scent: "Coconut & Mango", color: "Plain")
+bubble_candle_11 = Product.create(category: "Bubble Candle", scent: "Vanilla Rose", color: "Pastel Pink")
+bubble_candle_12 = Product.create(category: "Bubble Candle", scent: "Peppermint Vanilla", color: "Pastel Green")
+bubble_candle_13 = Product.create(category: "Bubble Candle", scent: "Peppermint Vanilla", color: "Plain")
+bubble_candle_14 = Product.create(category: "Bubble Candle", scent: "Warm Vanilla Kiss")
+bubble_candle_15 = Product.create(category: "Bubble Candle", scent: "Apple Cider")
+bubble_candle_16 = Product.create(category: "Bubble Candle", scent: "Apple Cinnamon Spice", color: "Pastel Tan")
+bubble_candle_17 = Product.create(category: "Bubble Candle", scent: "Vanilla Latte", color: "Pastel Tan")
+bubble_candle_18 = Product.create(category: "Bubble Candle", scent: "Vanilla Latte", color: "Plain")
+bubble_candle_19 = Product.create(category: "Bubble Candle", scent: "Brown Sugar Plum")
+bubble_candle_20 = Product.create(category: "Bubble Candle", scent: "Cinnamon Pumpkin Pie")
+bubble_candle_21 = Product.create(category: "Bubble Candle", scent: "Pumpkin & Vanilla")
+bubble_candle_22 = Product.create(category: "Bubble Candle", scent: "Gingerbread", color: "Pastel Tan")
+bubble_candle_23 = Product.create(category: "Bubble Candle", scent: "Gingerbread", color: "Plain")
+bubble_candle_24 = Product.create(category: "Bubble Candle", scent: "Pumpkin Spice Latte", color: "Tint of Orange")
+bubble_candle_25 = Product.create(category: "Bubble Candle", scent: "Pumpkin Spice Latte", color: "Plain")
